1ad64feabfa88289ef28b01b260ba29ec2197eea
Hisham <hishambutt9@gmail.com>
2020-12-01 23:41:53 +0500

    folderss

---
+++ clientreactt/src/Test.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import io from "socket.io-client";
+
+const fun=()=>{
+
+    io.emit('send_message',"123")
+  };
+
+
+class Test extends Component {
+  
+
+  componentDidMount = async () => {
+    //socket.io connection
+    const socket = io('http://localhost:5000/',{query:{id:'123'}});
+    // const socket = io({path:'http://localhost:5000/'});
+
+    socket.emit('send_message',"123")
+
+    socket.on("receive_message", (message) => {
+      console.log(message);
+    });
+
+    // socket.on("deletedThought", (id) => {
+    //   const updatedThoughts = this.state.thoughts.filter((thought) => {
+    //     return thought._id !== id;
+    //   });
+
+    //   this.setState({ thoughts: updatedThoughts });
+    // });
+
+    // socket.on("thoughtsCleared", () => {
+    //   this.setState({ thoughts: [] });
+    // });
+
+    
+  };
+
+  
+  
+  render() {
+
+    return (
+      <div><button onClick={()=>fun()}>Hello</button>
+
+      </div>
+    );
+  }
+}
+
+
+export default Test;
---
+++ clientvue/src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>Hello</div>
+</template>
+
+<script>
+import io from "socket.io-client";
+
+export default {
+
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  mounted() {
+
+   const socket = io('http://localhost:5000/',{query:{id:'123'}});
+    // const socket = io({path:'http://localhost:5000/'});
+
+    socket.emit('send_message',"123")
+
+    socket.on("receive_message", (message) => {
+      console.log(message);
+    });
+
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
