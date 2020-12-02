d40cd9ef5557337abadc1d2942787f4a558b3768
Hisham <hishambutt9@gmail.com>
2020-12-02 22:11:28 +0500

    changes

---
+++ changestreams/message.js
@@ -0,0 +1,31 @@
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+  getAllUsers
+} = require('../utils/users');
+
+const message= (conn,io)=>{
+    const test = conn.collection("test").watch();
+  
+    test.on("change", (change) => {
+      console.log(change.operationType);
+      switch (change.operationType) {
+        case "insert":
+          console.log(change.fullDocument);
+          io.to('123').emit('receive_message', change.fullDocument)
+          break;
+        case "replace":
+          console.log(change.fullDocument);
+          users=getAllUsers();
+          io.to('123').emit('receive_message', users);
+          break;
+        case "delete":
+          console.log(change.fullDocument);
+          break;
+      }
+    });
+  
+}
+module.exports = message;
--- server.js
+++ server.js
@@ -2,42 +2,61 @@ const express = require('express');
 var cors = require('cors')
 const app = express();
 const http = require('http');
+const message=require('./changestreams/message');
+const mongoose = require('mongoose');
+const config = require('config');
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+  getAllUsers
+} = require('./utils/users');
+
+
+const db = config.get('mongoURI');
+const PORT = process.env.PORT || 5000;
 
 //Init Middleware
 app.use(cors())
 app.use(express.json({ extented: false }));
-const PORT = process.env.PORT || 5000;
-
 
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
     cors: {
-      origin: "http://localhost:8080",
+      origin: ["http://localhost:8080","http://localhost:3000"],
       methods: ["GET", "POST"]
     }
   });
 
+
 io.on("connection", (socket) => {
     console.log("socket.io: User id: ", socket.handshake.query.id);
     socket.join(socket.handshake.query.id);
-
-
-    socket.on('send_message', message => {
-        console.log("Message",message);
+    userJoin('1','hisham',socket.id);
     
-        //Send message to only that particular room
-        io.to(message).emit('receive_message', {
-            'content': "content",
-            'senderChatID': "senderChatID",
-            'receiverChatID':"receiverChatID",
-        })
-    })
-
     socket.on("disconnect", () => {
       console.log("socket.io: User disconnected: ", socket.id);
     });
   });
 
+//connect to db
+mongoose.connect(process.env.DB_URI || db, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("MongoDB database connected");
+  console.log("Setting change streams");
+  message(connection,io);
+
+});
   
 
 server.listen(PORT, () => console.log(`Sever Started on Port ${PORT}`));
+
